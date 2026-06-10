50a43d8cad1c6343ea035ceca5e65b7a032406b7
brucewang <brucewang5638@gmail.com>
2026-06-10 10:26:45 +0800

    fix: 修复cube.js的环境变量导出

--- abacusflow-infra/abacusflow-cube/cube.js
+++ abacusflow-infra/abacusflow-cube/cube.js
@@ -1,2 +1,8 @@
 module.exports = {
+  jwt: {
+    jwkUrl: process.env.CUBEJS_JWT_JWK_URL,
+    audience: process.env.CUBEJS_JWT_AUDIENCE,
+    issuer: [process.env.CUBEJS_JWT_ISSUER],
+    algorithms: ["RS256"],
+  },
 };
